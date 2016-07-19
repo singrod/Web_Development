39778844c798b918adc1649faec593da3055c30d
singrod <singrod@users.noreply.github.com>
2016-07-19 17:50:26 -0400

    Added additional pages and content

---
+++ contact.php
@@ -0,0 +1,148 @@
+<?php require_once("includes/session.php"); ?>
+<?php require_once("includes/db_connection.php"); ?>
+<?php require_once("includes/functions.php"); ?>
+<?php require_once("includes/validation_functions.php"); ?>
+
+<?php 
+	if(true){
+		// category ID was missing or invalid or
+		// category couldn't be found in database
+		//redirect_to("index.php");
+	}
+?>
+
+<?php
+/* Process Form Info	
+_______________________________________________________________________________*/
+
+if(isset($_POST["submit"])){
+	// Details to be sent in email
+	$firstName = mysql_prep($_POST["firstName"]);
+	$lastName = mysql_prep($_POST["lastName"]);
+	$email_from = mysql_prep($_POST["email"]);
+	$phone = mysql_prep($_POST["phone"]);
+	$customerMessage = mysql_prep($_POST["customerMessage"]);
+	// validations
+	$required_fields = array("firstName", "lastName", "email");	
+	validate_presences($required_fields);
+	
+	$fields_with_max_lengths = array("firstName" => 30);
+	validate_max_lengths($fields_with_max_lengths);
+
+	$fields_with_max_lengths = array("lastName" => 30);
+	validate_max_lengths($fields_with_max_lengths);
+	
+	validate_email($email);
+	
+	if(empty($errors)){
+		
+		// Represent the destination and subject; adjust as needed
+		$email_to = "msingleton29@cfl.rr.com"; 
+		$email_subject = "Website Info Request!";		
+	
+		// Create email string
+		$email_message = "Request details below.\r\n\r\n";
+		$email_message .= "First Name: " . urldecode(clean_string($firstName)) . "\r\n";
+		$email_message .= "Last Name: " . urldecode(clean_string($lastName)) . "\r\n";
+		$email_message .= "Email: " . clean_string($email_from) . "\r\n"; 
+		$email_message .= "Phone: " . clean_string($phone) . "\r\n"; 
+		$email_message .= "Message: " . urldecode(clean_string($customerMessage)) . "\r\n";
+
+		// Receipt address
+		//$receipt_email_to = $email_from;
+		//$receipt_email_from = $email_to;
+		//$receipt_subject = "Order receipt from Tasha's Kitchen!";
+		
+		// Create email headers 
+		$headers = 'From: '. $email_from . "\r\n" . 'Reply-To: '. $email_from . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+		
+		//$receipt_headers = 'From: '. $receipt_email_from . "\r\n" . 'Reply-To: '. $receipt_email_from . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+		
+		// Send email 
+		mail($email_to, $email_subject, $email_message, $headers);
+		// Send receipt
+		//mail($receipt_email_to, $receipt_subject, $email_message, $receipt_headers);	
+		redirect_to("index.html");
+		
+	} else {
+		 echo message(); 
+		 echo form_errors($errors); 
+
+	} // empty($errors)
+} else { 
+	// This is probably a GET request
+} // end: isset($_POST["submit"] 
+
+/* End Process 
+_______________________________________________________________________________*/
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W12C//DTD XHTML 1.0 Transitional//EN" "http://www.w12.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w12.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>Contact</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+</head>
+<body>
+
+<div class="jumbotron">
+	<h1>Mark's Web Development</h1>
+</div>
+
+<nav class="navbar navbar-inverse" role="navigation">
+	<div class="container">
+		<div class="navbar-header">			
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span> 
+			</button>			
+			<a href="#" class="navbar-brand">Spread Your Word!</a>				
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="index.html" >Home</a></li>
+				<li><a href="products.php">Products</a></li>
+				<li><a href="about.html">About</a></li>
+				<li class="active"><a href="#">Contact</a></li>
+			</ul>
+		</div>		
+	</div>
+</nav>
+
+<div class="container" id="main">		
+	<div class="row">
+		<div class="container col-sm-12">
+			<img class="img-responsive" id="contactImage" src="images/pc-guy4.png" />
+		</div>
+		<div id="contact" class="col-sm-4">			
+			<h2>Direct Contact</h2>
+			<p>(800) 111-2222</p>
+			<p>msingleton29@cfl.rr.com</p>
+		</div>
+		<div id="contact" class="col-sm-8">			
+			<form action="contact.php" method="post">
+				<h2>Contact Me Today!</h2>
+				<br />
+				<input type="text" name="firstName" placeholder="First Name *" value=""required /><br /><br />
+				<input type="text" name="lastName" placeholder="Last Name *" value="" required/><br /><br />
+				<input type="email" name="email" placeholder="Email *" value="" required/><br /><br />
+				<input type="text" name="phone" placeholder="Phone" value="" /><br /><br />
+				<textarea rows="5" cols="57" name="customerMessage" placeholder="Message" value=""></textarea><br /><br />
+				<input class="btn" type="submit" name="submit" value="Submit" />
+			</form>			
+		</div>	
+	</div>
+	<footer class="col-sm-12 copyright">
+		<p>&copy; 2016. All Rights Reserved. Powered by Mark's Web Development.</p>
+	</footer>	
+</div>
+
+<script src="scripts/jquery-2.2.3.min.js"></script>
+<script src="scripts/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ products.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W12C//DTD XHTML 1.0 Transitional//EN" "http://www.w12.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w12.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>Products</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+</head>
+<body>
+
+<div class="jumbotron">
+	<h1>Mark's Web Development</h1>
+</div>
+
+<nav class="navbar navbar-inverse" role="navigation">
+	<div class="container">
+		<div class="navbar-header">			
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span> 
+			</button>			
+			<a href="#" class="navbar-brand">Spread Your Word!</a>				
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="index.html" >Home</a></li>
+				<li class="active"><a href="#">Products</a></li>
+				<li><a href="about.html">About</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+		</div>		
+	</div>
+</nav>
+
+<div class="container" id="main">		
+	<div class="row">
+		<div class="container col-sm-12">
+			<img class="img-responsive" id="productsImage" src="images/photo.jpg" />
+		</div>		
+		<div id="home" class="col-sm-4">			
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et mollis ligula. Vivamus ultrices, nulla et consectetur elementum, mi turpis aliquam lacus, ut feugiat nulla ipsum ut dolor. Duis ut est in dui vehicula placerat. Proin ut orci faucibus, ultrices dolor nec, tristique enim. Quisque eleifend ante ac ipsum rutrum, sed lobortis nunc suscipit. Quisque egestas dolor orci, et egestas velit rutrum vel. Vivamus eget convallis nunc. Donec suscipit, tellus vel ultricies feugiat, eros ex sollicitudin ex, quis dictum nisi neque eget quam. Sed nibh sapien, faucibus aliquam leo id, vestibulum feugiat tortor. Cras vitae orci lectus. Praesent ultricies consequat neque, sed gravida purus volutpat quis. Suspendisse consequat nulla vehicula, ullamcorper dui et, tincidunt erat. Ut porttitor ultricies enim sit amet finibus. Vivamus non mollis leo, eget tempus mi. In consectetur massa nec tincidunt congue.</p>			
+		</div>
+		<div id="home" class="col-sm-4">			
+			<p>Curabitur viverra ligula ac ultrices feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac mauris in est fermentum pulvinar. Aenean vitae erat non augue viverra laoreet. Integer hendrerit purus vel consectetur imperdiet. Aliquam commodo sem lorem, sit amet maximus arcu faucibus eget. Nunc tristique sapien vel turpis blandit tincidunt. Nullam nec enim vel lectus fermentum commodo eget quis turpis. Sed eu maximus urna, sed venenatis erat. Morbi posuere diam sed justo porttitor, nec aliquet lorem iaculis. Nullam et orci lobortis, mollis arcu ac, vulputate dolor.</p>			
+		</div>
+		<div id="home" class="col-sm-4">			
+			<p>Duis ac augue id metus semper consequat. Phasellus in est eget ex gravida placerat. Vivamus varius nulla eget est venenatis, at porta neque pretium. Phasellus sit amet felis tellus. Praesent sed neque vel nulla sagittis pretium. Sed nec laoreet eros. Nunc ante nunc, sagittis in magna in, accumsan imperdiet nisi. Maecenas sed sapien id risus accumsan facilisis. Praesent a magna sed odio dictum commodo sit amet non erat. Etiam justo urna, ullamcorper et quam iaculis, eleifend tincidunt lacus. Donec ut nisi consectetur, gravida ante sed, condimentum odio. Integer eget lacus eu lectus facilisis egestas.</p>			
+		</div>
+	</div>
+	<footer class="col-sm-12 copyright">
+		<p>&copy; 2016. All Rights Reserved. Powered by Mark's Web Development.</p>
+	</footer>	
+</div>
+
+<script src="scripts/jquery-2.2.3.min.js"></script>
+<script src="scripts/bootstrap.min.js"></script>
+</body>
+</html>
